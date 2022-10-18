9249368e7e9ffbce4f3b4e6e4f213a5e7cf3b060
Sebastian Lindner <sebastian.lindner@tuhh.de>
2022-10-18 17:26:09 +0200

    increase num cores

--- scenarios/results/s_sh-mac-aloha-25.sh
+++ scenarios/results/s_sh-mac-aloha-25.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 #SBATCH -p smp
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task 12
+#SBATCH --cpus-per-task 16
 #SBATCH --mem-per-cpu 2G
 #SBATCH --time 0-12:00:00
 #SBATCH --constraint=OS8
 
 # Execute simulation
-make sh-mac-aloha-25 NUM_CPUS=12
+make sh-mac-aloha-25 NUM_CPUS=16
 
 # Exit job
 exit
