03e192c2c54005dc9efcbc18c06b032231e333fe
Sebastian Lindner <sebastian.lindner@tuhh.de>
2022-10-18 17:27:23 +0200

    only need 1 core

--- scenarios/results/s_pp-voice.sh
+++ scenarios/results/s_pp-voice.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 #SBATCH -p smp
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task 20
+#SBATCH --cpus-per-task 1
 #SBATCH --mem-per-cpu 5G
 #SBATCH --time 5-00:00:00
 #SBATCH --constraint=OS8
 
 # Execute simulation
-make pp-voice NUM_CPUS=20
+make pp-voice NUM_CPUS=1
 
 # Exit job
 exit
