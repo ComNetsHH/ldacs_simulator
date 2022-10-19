3f05b7fa214c5689ed2d49deb5846d631aa78463
Sebastian Lindner <sebastian.lindner@tuhh.de>
2022-10-19 14:20:21 +0200

    update targets

--- scenarios/results/s_broadcast-voice.sh
+++ scenarios/results/s_broadcast-voice.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 #SBATCH -p smp
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task 20
-#SBATCH --mem-per-cpu 4G
+#SBATCH --cpus-per-task 16
+#SBATCH --mem-per-cpu 2G
 #SBATCH --time 0-06:00:00
 #SBATCH --constraint=OS8
 
 # Execute simulation
-make broadcast-voice NUM_CPUS=20
+make broadcast-voice NUM_CPUS=16
 
 # Exit job
 exit
--- scenarios/results/s_mac-aloha.sh
+++ scenarios/results/s_mac-aloha.sh
@@ -3,11 +3,11 @@
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 16
 #SBATCH --mem-per-cpu 2G
-#SBATCH --time 0-12:00:00
+#SBATCH --time 5-00:00:00
 #SBATCH --constraint=OS8
 
 # Execute simulation
-make sh-mac-aloha-75 NUM_CPUS=16
+make sh-mac-aloha NUM_CPUS=16
 
 # Exit job
 exit
