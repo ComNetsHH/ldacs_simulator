b3a669bf69da9d5c575cf98150ffb7c130acb57e
Sebastian Lindner <sebastian.lindner@tuhh.de>
2022-11-01 10:14:49 +0100

    use 14 cores as Xeon CPUs have 14 cores

--- scenarios/results/s_pp-link-estbl.sh
+++ scenarios/results/s_pp-link-estbl.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 #SBATCH -p smp
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task 16
+#SBATCH --cpus-per-task 14
 #SBATCH --mem-per-cpu 2G
 #SBATCH --time 5-00:00:00
 #SBATCH --constraint=OS8
 
 # Execute simulation
-make pp-link-estbl-test NUM_CPUS=16
+make pp-link-estbl-test NUM_CPUS=14
 
 # Exit job
 exit
--- scenarios/results/s_sh-mac-mcsotdma-opt.sh
+++ scenarios/results/s_sh-mac-mcsotdma-opt.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -l
 #SBATCH -p smp
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task 16
+#SBATCH --cpus-per-task 14
 #SBATCH --mem-per-cpu 2G
 #SBATCH --time 0-12:00:00
 #SBATCH --constraint=OS8
 
 # Execute simulation
-make sh-mac-mcsotdma-opt NUM_CPUS=16
+make sh-mac-mcsotdma-opt NUM_CPUS=14
+make sh-mac-mcsotdma-all_plot
 
 # Exit job
 exit
