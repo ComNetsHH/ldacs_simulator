656d0f93a7cfc9c20ebca59ef585f6bb69af6276
Sebastian Lindner <sebastian.lindner@tuhh.de>
2022-11-11 15:37:37 +0100

    configure num links for broadcast voice + link estbl

--- scenarios/results/s_pp-link-estbl.sh
+++ scenarios/results/s_pp-link-estbl.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 #SBATCH -p smp
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task 14
-#SBATCH --mem-per-cpu 2G
+#SBATCH --cpus-per-task 8
+#SBATCH --mem-per-cpu 8G
 #SBATCH --time 5-00:00:00
 #SBATCH --constraint=OS8
 
 # Execute simulation
-make pp-link-estbl NUM_CPUS=14
+make pp-link-estbl NUM_CPUS=8
 
 # Exit job
 exit
