9d075a76951122a0ee78182dbb19e49575c8e4a8
Sebastian Lindner <sebastian.lindner@tuhh.de>
2022-11-11 13:07:19 +0100

    update slurm scripts

--- scenarios/results/s_mac-mcsotdma-opt.sh
+++ scenarios/results/s_mac-mcsotdma-opt.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 #SBATCH -p smp
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task 14
+#SBATCH --cpus-per-task 8
 #SBATCH --mem-per-cpu 8G
 #SBATCH --time 2-00:00:00
 #SBATCH --constraint=OS8
 
 # Execute simulation
-make sh-mac-mcsotdma-opt NUM_CPUS=14
+make sh-mac-mcsotdma-opt NUM_CPUS=8
 
 # Exit job
 exit
