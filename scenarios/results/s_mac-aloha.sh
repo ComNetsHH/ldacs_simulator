7aafdcb1a60757f0a538282958a50482ae3b6363
Sebastian Lindner <sebastian.lindner@tuhh.de>
2022-11-11 09:04:56 +0100

    sync num_cpus to others

--- scenarios/results/s_mac-aloha.sh
+++ scenarios/results/s_mac-aloha.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 #SBATCH -p smp
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task 10
+#SBATCH --cpus-per-task 8
 #SBATCH --mem-per-cpu 8G
 #SBATCH --time 0-12:00:00
 #SBATCH --constraint=OS8
 
 # Execute simulation
-make sh-mac-aloha NUM_CPUS=10
+make sh-mac-aloha NUM_CPUS=8
 
 # Exit job
 exit
