14fe84ac92df9e85987ead09e0d633285866ed8a
Sebastian Lindner <sebastian.lindner@tuhh.de>
2022-11-14 14:30:07 +0100

    not as long, fewer reps

--- scenarios/results/s_mac-mcsotdma-opt-slotadv.sh
+++ scenarios/results/s_mac-mcsotdma-opt-slotadv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 #SBATCH -p smp
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task 10
-#SBATCH --mem-per-cpu 12G
+#SBATCH --cpus-per-task 6
+#SBATCH --mem-per-cpu 16G
 #SBATCH --time 3-00:00:00
 #SBATCH --constraint=OS8
 
 # Execute simulation
-make sh-mac-mcsotdma-opt-slotadv NUM_CPUS=10
+make sh-mac-mcsotdma-opt-slotadv NUM_CPUS=6
 
 # Exit job
 exit
--- scenarios/results/s_pp-link-estbl.sh
+++ scenarios/results/s_pp-link-estbl.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 #SBATCH -p smp
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task 8
+#SBATCH --cpus-per-task 10
 #SBATCH --mem-per-cpu 8G
-#SBATCH --time 5-00:00:00
+#SBATCH --time 1-00:00:00
 #SBATCH --constraint=OS8
 
 # Execute simulation
-make pp-link-estbl NUM_CPUS=8
+make pp-link-estbl NUM_CPUS=10
 
 # Exit job
 exit
