f8b63255425f01e919dcae4d14d770d12180b4d8
Sebastian Lindner <sebastian.lindner@tuhh.de>
2022-11-14 10:05:17 +0100

    new link estbl sims

--- scenarios/results/eval_link_estbl_time.py
+++ scenarios/results/eval_link_estbl_time.py
@@ -12,6 +12,7 @@ import progressbar
 
 json_label_reps = 'num_reps'
 json_label_num_nodes_vec = 'num_nodes_vec'
+json_label_max_num_pp_links = 'max_num_pp_links'
 json_label_num_broadcast_nodes_vec = 'num_broadcast_nodes_vec'
 json_label_link_estbl_time_mat = 'link_estbl_time_vec'
 json_label_broadcast_mac_delay_mat = 'broadcast_mac_delay_mat'
@@ -25,10 +26,10 @@ def calculate_confidence_interval(data, confidence):
 	return [m, m - h, m + h]
 
 
-def parse(dir, num_nodes_vec, num_broadcast_nodes_vec, num_reps, json_filename):		
-	link_estbl_time_mat = np.zeros((len(num_nodes_vec), len(num_broadcast_nodes_vec), num_reps))
-	broadcast_mac_delay_mat = np.zeros((len(num_nodes_vec), len(num_broadcast_nodes_vec), num_reps))	
-	bar_max_i = num_reps * len(num_nodes_vec) * len(num_broadcast_nodes_vec)
+def parse(dir, num_nodes_vec, num_broadcast_nodes_vec, max_num_pp_links, num_reps, json_filename):		
+	link_estbl_time_mat = np.zeros((len(num_nodes_vec), len(num_broadcast_nodes_vec), len(max_num_pp_links), num_reps))
+	broadcast_mac_delay_mat = np.zeros((len(num_nodes_vec), len(num_broadcast_nodes_vec), len(max_num_pp_links), num_reps))	
+	bar_max_i = num_reps * len(num_nodes_vec) * len(num_broadcast_nodes_vec) * len(max_num_pp_links)
 	bar_i = 0
 	print('parsing ' + str(bar_max_i) + ' result files')
 	bar = progressbar.ProgressBar(max_value=bar_max_i, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
@@ -39,48 +40,52 @@ def parse(dir, num_nodes_vec, num_broadcast_nodes_vec, num_reps, json_filename):
 		# for each no of broadcast users
 		for j in range(len(num_broadcast_nodes_vec)):
 			m = num_broadcast_nodes_vec[j]			
-			# for each repetition
-			for rep in range(num_reps):
-				try:
-					filename = dir + '/n=' + str(n) + ',m=' + str(m) + '-#' + str(rep) 
-					filename_sca = filename + '.sca.csv'
-					filename_vec = filename + '.vec.csv'
-					results_sca = pd.read_csv(filename_sca)
-					results_vec = pd.read_csv(filename_vec)					
+			# for each no of PP links
+			for k in range(len(max_num_pp_links)):
+				l = max_num_pp_links[k]
+				# for each repetition
+				for rep in range(num_reps):
+					try:
+						filename = dir + '/n=' + str(n) + ',m=' + str(m) + ',l=' + str(l) + '-#' + str(rep) 
+						filename_sca = filename + '.sca.csv'
+						filename_vec = filename + '.vec.csv'
+						results_sca = pd.read_csv(filename_sca)
+						results_vec = pd.read_csv(filename_vec)					
 
-					# get link establishment time
-					establishment_time_vec = np.zeros(n)
-					broadcast_mac_delay_vec = np.zeros(n)
-					for node in range(n):					
-						num_links_estbl = int(results_sca[(results_sca.type=='scalar') & (results_sca.module=='NW_LINK_ESTABLISHMENT.txNode[' + str(node) + '].wlan[0].linkLayer') & (results_sca.name=='mcsotdma_statistic_num_pp_links_established:last')].value)
-						results_vec_link_estbl_time = None
-						if num_links_estbl > 0:														
-							results_vec_link_estbl_time = results_vec[(results_vec.type=='vector') & (results_vec.name=='mcsotdma_statistic_pp_link_establishment_time:vector') & (results_vec.module=='NW_LINK_ESTABLISHMENT.txNode[' + str(node) + '].wlan[0].linkLayer')]
-							results_vec_link_estbl_time = results_vec_link_estbl_time['vecvalue']
-							results_vec_link_estbl_time = [float(s) for s in results_vec_link_estbl_time.values[0].split(' ')]						
-						else:
-							print('zero links established in ' + filename)
+						# get link establishment time
+						establishment_time_vec = np.zeros(n)
+						broadcast_mac_delay_vec = np.zeros(n)
+						for node in range(n):					
+							num_links_estbl = int(results_sca[(results_sca.type=='scalar') & (results_sca.module=='NW_LINK_ESTABLISHMENT.txNode[' + str(node) + '].wlan[0].linkLayer') & (results_sca.name=='mcsotdma_statistic_num_pp_links_established:last')].value)
+							results_vec_link_estbl_time = None
+							if num_links_estbl > 0:														
+								results_vec_link_estbl_time = results_vec[(results_vec.type=='vector') & (results_vec.name=='mcsotdma_statistic_pp_link_establishment_time:vector') & (results_vec.module=='NW_LINK_ESTABLISHMENT.txNode[' + str(node) + '].wlan[0].linkLayer')]
+								results_vec_link_estbl_time = results_vec_link_estbl_time['vecvalue']
+								results_vec_link_estbl_time = [float(s) for s in results_vec_link_estbl_time.values[0].split(' ')]						
+							else:
+								print('zero links established in ' + filename)
 
-						results_vec_broadcast_mac_delay = results_vec[(results_vec.type=='vector') & (results_vec.name=='mcsotdma_statistic_broadcast_mac_delay:vector') & (results_vec.module=='NW_LINK_ESTABLISHMENT.txNode[' + str(node) + '].wlan[0].linkLayer')]
-						results_vec_broadcast_mac_delay = results_vec_broadcast_mac_delay['vecvalue']
-						results_vec_broadcast_mac_delay = [float(s) for s in results_vec_broadcast_mac_delay.values[0].split(' ')]
+							results_vec_broadcast_mac_delay = results_vec[(results_vec.type=='vector') & (results_vec.name=='mcsotdma_statistic_broadcast_mac_delay:vector') & (results_vec.module=='NW_LINK_ESTABLISHMENT.txNode[' + str(node) + '].wlan[0].linkLayer')]
+							results_vec_broadcast_mac_delay = results_vec_broadcast_mac_delay['vecvalue']
+							results_vec_broadcast_mac_delay = [float(s) for s in results_vec_broadcast_mac_delay.values[0].split(' ')]
 
-						# compute means
-						establishment_time_vec[node] = np.mean(results_vec_link_estbl_time)
-						broadcast_mac_delay_vec[node] = np.mean(results_vec_broadcast_mac_delay)
-					link_estbl_time_mat[i, j, rep] = np.mean(establishment_time_vec)
-					broadcast_mac_delay_mat[i, j, rep] = np.mean(broadcast_mac_delay_vec)
-					
-					bar_i += 1
-					bar.update(bar_i)
-				except FileNotFoundError as err:
-					print(err)			
+							# compute means
+							establishment_time_vec[node] = np.mean(results_vec_link_estbl_time)
+							broadcast_mac_delay_vec[node] = np.mean(results_vec_broadcast_mac_delay)
+						link_estbl_time_mat[i, j, k, rep] = np.mean(establishment_time_vec)
+						broadcast_mac_delay_mat[i, j, k, rep] = np.mean(broadcast_mac_delay_vec)
+						
+						bar_i += 1
+						bar.update(bar_i)
+					except FileNotFoundError as err:
+						print(err)			
 	bar.finish()				
 
 	# Save to JSON.
 	json_data = {}
 	json_data[json_label_reps] = num_reps
 	json_data[json_label_num_nodes_vec] = num_nodes_vec
+	json_data[json_label_max_num_pp_links] = max_num_pp_links
 	json_data[json_label_num_broadcast_nodes_vec] = num_broadcast_nodes_vec	
 	json_data[json_label_link_estbl_time_mat] = link_estbl_time_mat.tolist()	
 	json_data[json_label_broadcast_mac_delay_mat] = broadcast_mac_delay_mat.tolist()		
@@ -89,7 +94,7 @@ def parse(dir, num_nodes_vec, num_broadcast_nodes_vec, num_reps, json_filename):
 	print("Saved parsed results in '" + json_filename + "'.")    	
 
 
-def plot(json_filename, time_slot_duration, graph_filename_delay, graph_filename_stacked):
+def plot(json_filename, time_slot_duration, graph_filename_delay):
 	"""
 	Reads 'json_filename' and plots the values to 'graph_filename'.
 	"""
@@ -97,21 +102,23 @@ def plot(json_filename, time_slot_duration, graph_filename_delay, graph_filename
 		# Load JSON
 		json_data = json.load(json_file)
 		num_reps = json_data[json_label_reps]
+		max_num_pp_links = np.array(json_data[json_label_max_num_pp_links])
 		num_nodes_vec = np.array(json_data[json_label_num_nodes_vec])
 		num_broadcast_nodes_vec = np.array(json_data[json_label_num_broadcast_nodes_vec])
 		link_estbl_time_mat = np.array(json_data[json_label_link_estbl_time_mat])
 		broadcast_mac_delay_mat = np.array(json_data[json_label_broadcast_mac_delay_mat])
 		# Calculate confidence intervals
-		link_estbl_time_means = np.zeros((len(num_nodes_vec), len(num_broadcast_nodes_vec)))
-		link_estbl_time_err = np.zeros((len(num_nodes_vec), len(num_broadcast_nodes_vec)))	
-		broadcast_mac_delay_means = np.zeros((len(num_nodes_vec), len(num_broadcast_nodes_vec)))
-		broadcast_mac_delay_err = np.zeros((len(num_nodes_vec), len(num_broadcast_nodes_vec)))	
+		link_estbl_time_means = np.zeros((len(num_nodes_vec), len(num_broadcast_nodes_vec), len(max_num_pp_links)))
+		link_estbl_time_err = np.zeros((len(num_nodes_vec), len(num_broadcast_nodes_vec), len(max_num_pp_links)))	
+		broadcast_mac_delay_means = np.zeros((len(num_nodes_vec), len(num_broadcast_nodes_vec), len(max_num_pp_links)))
+		broadcast_mac_delay_err = np.zeros((len(num_nodes_vec), len(num_broadcast_nodes_vec), len(max_num_pp_links)))	
 		for i in range(len(num_nodes_vec)):	
 			for j in range(len(num_broadcast_nodes_vec)):
-				link_estbl_time_means[i, j], _, time_p = calculate_confidence_interval(link_estbl_time_mat[i, j, :], confidence=.95)				
-				link_estbl_time_err[i, j] = time_p - link_estbl_time_means[i, j]
-				broadcast_mac_delay_means[i, j], _, delay_p = calculate_confidence_interval(broadcast_mac_delay_mat[i, j, :], confidence=.95)				
-				broadcast_mac_delay_err[i, j] = delay_p - broadcast_mac_delay_means[i, j]
+				for k in range(len(max_num_pp_links)):
+					link_estbl_time_means[i, j, k], _, time_p = calculate_confidence_interval(link_estbl_time_mat[i, j, k, :], confidence=.95)				
+					link_estbl_time_err[i, j, k] = time_p - link_estbl_time_means[i, j, k]
+					broadcast_mac_delay_means[i, j, k], _, delay_p = calculate_confidence_interval(broadcast_mac_delay_mat[i, j, k, :], confidence=.95)				
+					broadcast_mac_delay_err[i, j, k] = delay_p - broadcast_mac_delay_means[i, j, k]
 
 		plt.rcParams.update({
 			'font.family': 'serif',
@@ -122,31 +129,17 @@ def plot(json_filename, time_slot_duration, graph_filename_delay, graph_filename
 		})
 		# 1st graph: delay
 		fig = plt.figure()		
-		line = plt.errorbar(num_broadcast_nodes_vec, link_estbl_time_means[0, :]*time_slot_duration, link_estbl_time_err[0, :]*time_slot_duration, alpha=0.75, fmt='o')
-		plt.plot(num_broadcast_nodes_vec, link_estbl_time_means[0, :]*time_slot_duration, linestyle='--', linewidth=.5, color=line[0].get_color(), alpha=.75)
+		for k in range(len(max_num_pp_links)):
+			line = plt.errorbar(num_broadcast_nodes_vec, link_estbl_time_means[0, :, k]*time_slot_duration, link_estbl_time_err[0, :, k]*time_slot_duration, alpha=0.75, fmt='o', label='$l=' + str(max_num_pp_links[k]) + '$')
+			plt.plot(num_broadcast_nodes_vec, link_estbl_time_means[0, :, k]*time_slot_duration, linestyle='--', linewidth=.5, color=line[0].get_color(), alpha=.75)
 		plt.ylabel('Link establishment time [ms]')		
-		plt.xlabel('No. of neighbors')
-		# plt.legend(framealpha=0.0, prop={'size': 8}, loc='upper center', bbox_to_anchor=(.5, 1.35), ncol=3)		
+		plt.xlabel('Number of users $n$')
+		plt.legend(framealpha=0.0, prop={'size': 7}, loc='upper center', bbox_to_anchor=(.5, 1.35), ncol=2)		
 		fig.tight_layout()
 		settings.init()
 		fig.set_size_inches((settings.fig_width, settings.fig_height), forward=False)
 		fig.savefig(graph_filename_delay, dpi=500, bbox_inches = 'tight', pad_inches = 0.01)		
 		print("Graph saved to " + graph_filename_delay)    
-		plt.close()  
-
-		# 2nd graph: stacked bars of SH channel access delay and total link establishment delay
-		fig, ax = plt.subplots()
-		width = .35		
-		ax.bar(num_broadcast_nodes_vec - width/2, broadcast_mac_delay_means[0, :]*time_slot_duration, width, yerr=broadcast_mac_delay_err[0,:]*time_slot_duration, label='SH channel access')
-		ax.bar(num_broadcast_nodes_vec + width/2, link_estbl_time_means[0, :]*time_slot_duration, width, yerr=link_estbl_time_err[0,:]*time_slot_duration, label='link establishment time')
-		ax.set_ylabel('Milliseconds')
-		ax.set_xlabel('No. of neighbors')
-		ax.set_xticks(num_broadcast_nodes_vec)
-		plt.legend(framealpha=0.0, prop={'size': 8}, loc='upper center', bbox_to_anchor=(.5, 1.15), ncol=2)		
-		fig.tight_layout()
-		fig.set_size_inches((settings.fig_width, settings.fig_height), forward=False)
-		fig.savefig(graph_filename_stacked, dpi=500, bbox_inches = 'tight', pad_inches = 0.01)		
-		print("Graph saved to " + graph_filename_stacked)    
 		plt.close()  
 
 
@@ -158,7 +151,8 @@ if __name__ == "__main__":
 	parser.add_argument('--no_plot', action='store_true', help='Whether *not* to plot predictions errors from JSON results.')				
 	parser.add_argument('--n', type=int, nargs='+', help='Number of user pairs.', default=[1])	
 	parser.add_argument('--m', type=int, nargs='+', help='Number of broadcast users.', default=[0])	
-	parser.add_argument('--num_reps', type=int, help='Number of repetitions that should be considered.', default=1)	
+	parser.add_argument('--max_num_links', type=int, nargs='+', help='Maximum number of PP links that should be supported.', default=[1])	
+	parser.add_argument('--num_reps', type=int, help='Number of repetitions that should be considered.', default=1)		
 	parser.add_argument('--time_slot_duration', type=int, help='Time slot duration in milliseconds.', default=24)		
 
 	args = parser.parse_args()	
@@ -168,12 +162,11 @@ if __name__ == "__main__":
 		if not os.path.exists(dir):
 			os.makedirs(dir)
 		
-	output_filename_base = args.filename + '_n-' + str(args.n) + '_m-' + str(args.m) + "-rep" + str(args.num_reps)
+	output_filename_base = args.filename + '_n-' + str(args.n) + '_m-' + str(args.m) + '_l=' + str(args.max_num_links) + "-rep" + str(args.num_reps)
 	json_filename = "_data/" + output_filename_base + ".json"
-	graph_filename_delay = "_imgs/" + output_filename_base + "_delay.pdf"
-	graph_filename_stacked = "_imgs/" + output_filename_base + "_stacked.pdf"
+	graph_filename_delay = "_imgs/" + output_filename_base + "_delay.pdf"	
 	if not args.no_parse:		
-		parse(args.dir, args.n, args.m, args.num_reps, json_filename)
+		parse(args.dir, args.n, args.m, args.max_num_links, args.num_reps, json_filename)
 	if not args.no_plot:
-		plot(json_filename, args.time_slot_duration, graph_filename_delay, graph_filename_stacked) 
+		plot(json_filename, args.time_slot_duration, graph_filename_delay) 
     
